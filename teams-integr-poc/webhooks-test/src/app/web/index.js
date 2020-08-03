c8c5ae0c06e7eabf51efc597575de6f68ed0fc15
Evgenii Rosliakov <evgenii_rosliakov@epam.com>
2020-08-03 12:50:07 +0300

    cut out secrets

--- teams-integr-poc/webhooks-test/src/app/web/index.js
+++ teams-integr-poc/webhooks-test/src/app/web/index.js
@@ -1,10 +1,11 @@
+const incomingWebhookSecret = require('../config/incomingWebhook.secret.json');
+
 const socket = io();
 
 window.addEventListener('load', () => {
     console.log('loaded');
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const channelPath =
-        'https://outlook.office.com/webhook/20d5daf1-c36a-4332-80c7-6bb19418c7f5@b41b72d0-4e9f-4c26-8a69-f949f367c91d/IncomingWebhook/e536494906294c1fb5ae8008e1d8d878/df6c7012-9187-4374-b5f4-dbcdb9466de7';
+    const channelPath = JSON.parse(incomingWebhookSecret).channelPath;
 
     const incomingArea = document.querySelector('.incoming-area');
     const outgoingArea = document.querySelector('.outgoing-area');
@@ -25,5 +26,5 @@ window.addEventListener('load', () => {
         outgoingArea.value = '';
     });
 
-    socket.on('message', msg => incomingArea.value += `\n${msg}\n       ----------\n`);
+    socket.on('message', msg => incomingArea.value += `\n${msg}\n----------\n`);
 });
